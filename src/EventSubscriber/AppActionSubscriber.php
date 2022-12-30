2263a0bcbea0da1b59e11e89e2fade4bf5cd6eca
Anisimov Kostya <k.anisimov@adtelligent.com>
2022-12-30 08:03:40 +0200

    user. it was added simple user

---
+++ src/EventSubscriber/AppActionSubscriber.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+
+class AppActionSubscriber implements EventSubscriberInterface
+{
+    public function onKernelRequest(RequestEvent $event): void
+    {
+        // ...
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'kernel.request' => 'onKernelRequest',
+        ];
+    }
+}
